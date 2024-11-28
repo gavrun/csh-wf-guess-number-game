bf931028f6d66946e649cd2c90d9252748910149
gavrun <38539655+gavrun@users.noreply.github.com>
2024-11-28 17:20:14 +0000

    First draft

--- MainForm.Designer.cs
+++ MainForm.Designer.cs
@@ -32,11 +32,13 @@
             this.lblMessage = new System.Windows.Forms.Label();
             this.btnCheck = new System.Windows.Forms.Button();
             this.btnRestart = new System.Windows.Forms.Button();
+            this.pictureBoxLogo = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxLogo)).BeginInit();
             this.SuspendLayout();
             // 
             // txtGuess
             // 
-            this.txtGuess.Location = new System.Drawing.Point(131, 254);
+            this.txtGuess.Location = new System.Drawing.Point(29, 111);
             this.txtGuess.Name = "txtGuess";
             this.txtGuess.Size = new System.Drawing.Size(100, 20);
             this.txtGuess.TabIndex = 0;
@@ -45,7 +47,7 @@
             // lblMessage
             // 
             this.lblMessage.AutoSize = true;
-            this.lblMessage.Location = new System.Drawing.Point(128, 70);
+            this.lblMessage.Location = new System.Drawing.Point(26, 31);
             this.lblMessage.Name = "lblMessage";
             this.lblMessage.Size = new System.Drawing.Size(50, 13);
             this.lblMessage.TabIndex = 1;
@@ -53,7 +55,7 @@
             // 
             // btnCheck
             // 
-            this.btnCheck.Location = new System.Drawing.Point(533, 70);
+            this.btnCheck.Location = new System.Drawing.Point(29, 175);
             this.btnCheck.Name = "btnCheck";
             this.btnCheck.Size = new System.Drawing.Size(75, 23);
             this.btnCheck.TabIndex = 2;
@@ -63,7 +65,7 @@
             // 
             // btnRestart
             // 
-            this.btnRestart.Location = new System.Drawing.Point(533, 254);
+            this.btnRestart.Location = new System.Drawing.Point(215, 175);
             this.btnRestart.Name = "btnRestart";
             this.btnRestart.Size = new System.Drawing.Size(75, 23);
             this.btnRestart.TabIndex = 3;
@@ -71,17 +73,29 @@
             this.btnRestart.UseVisualStyleBackColor = true;
             this.btnRestart.Click += new System.EventHandler(this.btnRestart_Click);
             // 
+            // pictureBoxLogo
+            // 
+            this.pictureBoxLogo.Image = global::csh_wf_guess_number_game.Properties.Resources.logo;
+            this.pictureBoxLogo.Location = new System.Drawing.Point(190, 31);
+            this.pictureBoxLogo.Name = "pictureBoxLogo";
+            this.pictureBoxLogo.Size = new System.Drawing.Size(100, 100);
+            this.pictureBoxLogo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBoxLogo.TabIndex = 4;
+            this.pictureBoxLogo.TabStop = false;
+            // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(323, 223);
+            this.Controls.Add(this.pictureBoxLogo);
             this.Controls.Add(this.btnRestart);
             this.Controls.Add(this.btnCheck);
             this.Controls.Add(this.lblMessage);
             this.Controls.Add(this.txtGuess);
             this.Name = "MainForm";
             this.Text = "MainForm";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxLogo)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -93,6 +107,7 @@
         private System.Windows.Forms.Label lblMessage;
         private System.Windows.Forms.Button btnCheck;
         private System.Windows.Forms.Button btnRestart;
+        private System.Windows.Forms.PictureBox pictureBoxLogo;
     }
 }
 
