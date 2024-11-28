b51404d46534cb3099cbdb74b8d15460fb009b48
gavrun <38539655+gavrun@users.noreply.github.com>
2024-11-28 17:00:54 +0000

    Add project files.

---
+++ MainForm.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace csh_wf_guess_number_game
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtGuess = new System.Windows.Forms.TextBox();
+            this.lblMessage = new System.Windows.Forms.Label();
+            this.btnCheck = new System.Windows.Forms.Button();
+            this.btnRestart = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtGuess
+            // 
+            this.txtGuess.Location = new System.Drawing.Point(131, 254);
+            this.txtGuess.Name = "txtGuess";
+            this.txtGuess.Size = new System.Drawing.Size(100, 20);
+            this.txtGuess.TabIndex = 0;
+            this.txtGuess.Text = "Guess";
+            // 
+            // lblMessage
+            // 
+            this.lblMessage.AutoSize = true;
+            this.lblMessage.Location = new System.Drawing.Point(128, 70);
+            this.lblMessage.Name = "lblMessage";
+            this.lblMessage.Size = new System.Drawing.Size(50, 13);
+            this.lblMessage.TabIndex = 1;
+            this.lblMessage.Text = "Message";
+            // 
+            // btnCheck
+            // 
+            this.btnCheck.Location = new System.Drawing.Point(533, 70);
+            this.btnCheck.Name = "btnCheck";
+            this.btnCheck.Size = new System.Drawing.Size(75, 23);
+            this.btnCheck.TabIndex = 2;
+            this.btnCheck.Text = "Check";
+            this.btnCheck.UseVisualStyleBackColor = true;
+            this.btnCheck.Click += new System.EventHandler(this.btnCheck_Click);
+            // 
+            // btnRestart
+            // 
+            this.btnRestart.Location = new System.Drawing.Point(533, 254);
+            this.btnRestart.Name = "btnRestart";
+            this.btnRestart.Size = new System.Drawing.Size(75, 23);
+            this.btnRestart.TabIndex = 3;
+            this.btnRestart.Text = "Restart";
+            this.btnRestart.UseVisualStyleBackColor = true;
+            this.btnRestart.Click += new System.EventHandler(this.btnRestart_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnRestart);
+            this.Controls.Add(this.btnCheck);
+            this.Controls.Add(this.lblMessage);
+            this.Controls.Add(this.txtGuess);
+            this.Name = "MainForm";
+            this.Text = "MainForm";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtGuess;
+        private System.Windows.Forms.Label lblMessage;
+        private System.Windows.Forms.Button btnCheck;
+        private System.Windows.Forms.Button btnRestart;
+    }
+}
+
---
+++ MainForm.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace csh_wf_guess_number_game
+{
+    public partial class MainForm : Form
+    {
+        private int randomNumber;
+        private int attempts;
+
+
+        public MainForm()
+        {
+            InitializeComponent();
+
+            StartGame();
+        }
+
+        private void StartGame()
+        {
+            Random rnd = new Random();
+            randomNumber = rnd.Next(1, 101);
+
+            attempts = 0;
+
+            lblMessage.Text = "Guess number from 1 to 100!";
+
+            txtGuess.Clear();
+            txtGuess.Focus();
+        }
+
+        private void btnCheck_Click(object sender, EventArgs e)
+        {
+            if (int.TryParse(txtGuess.Text, out int userGuess))
+            {
+                attempts++;
+                if (userGuess < randomNumber)
+                {
+                    lblMessage.Text = "Number is bigger.";
+                }
+                else if (userGuess > randomNumber)
+                {
+                    lblMessage.Text = "Number is smaller.";
+                }
+                else
+                {
+                    MessageBox.Show($"Congrats! You guessed it for {attempts} attempts.", "Win");
+                    StartGame();
+                }
+            }
+            else
+            {
+                lblMessage.Text = "Input correct number.";
+            }
+        }
+
+        private void btnRestart_Click(object sender, EventArgs e)
+        {
+            StartGame();
+        }
+    }
+}
